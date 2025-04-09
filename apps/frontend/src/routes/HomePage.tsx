f1a9dd1495885c435773440deccbc1dc2245b809
keiraSchoolcraft <keira.schoolcraft@icloud.com>
2025-04-09 15:56:07 -0400

    slight change to home page so it fills the screen

--- apps/frontend/src/routes/HomePage.tsx
+++ apps/frontend/src/routes/HomePage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const HomePage = () => {
+
     return (
-        <div className="p-10">
+        <div className="p-10 h-screen">
             <h1 className="font-bold text-xl pb-4 text-center">Home Page</h1>
         </div>
     );
