b056d6d5136be65f349239c621acbdb85b784a64
keiraSchoolcraft <keira.schoolcraft@icloud.com>
2025-04-10 14:25:35 -0400

    get frontend skeleton to align

---
+++ apps/frontend/src/routes/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+
+    return (
+        <div className="p-10 h-screen">
+            <h1 className="font-bold text-xl pb-4 text-center">Home Page</h1>
+        </div>
+    );
+};
+
+export default HomePage;
