cff59642bcf96170b88dd0165865aa39cba6ac2a
keiraSchoolcraft <keira.schoolcraft@icloud.com>
2025-04-10 15:08:05 -0400

    add basic upload button

---
+++ apps/frontend/src/components/FileUpload.tsx
@@ -0,0 +1,47 @@
+import React, {useRef} from 'react';
+
+//the interface defines the props the component will accept
+interface FileUploadProps {
+    onFileSelected: (file: File | null) => void;
+    acceptedFileTypes?: string[];
+    // ^^ can give an array of strings of accepted file types (json, pdf, etc)
+    buttonText?: string;
+    //customize button text
+}
+
+//functional react component
+const FileUpload: React.FC<FileUploadProps> = ({ onFileSelected, acceptedFileTypes, buttonText}) => {
+    //create mutable reference to input type "file"
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    //for when the user selects or deselects the file
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files: FileList | null = event.target.files;
+        //check if files have been selected for handling
+        if(files && files.length > 0) {
+            const file: File = files[0]; //only handling first file
+            onFileSelected(file);
+        } else {
+            onFileSelected(null);
+        }
+    };
+
+    //function to handle button click (file selection)
+    const handleClick = () => {
+        if(fileInputRef.current) {
+            fileInputRef.current.click(); //trigger browser file selection dialog
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={handleClick} className="bg-black text-white rounded-lg px-5 py-1">{buttonText}</button>
+            <input type='file'
+                   accept={acceptedFileTypes ? acceptedFileTypes.join(','):undefined}
+                   onChange={handleFileChange}
+                   style={{display: 'none'}}
+                   ref={fileInputRef} />
+        </div>
+    );
+};
+
+export default FileUpload;
--- apps/frontend/src/routes/HomePage.tsx
+++ apps/frontend/src/routes/HomePage.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import FileUpload from "../components/FileUpload.tsx";
 
 const HomePage = () => {
-
+    //state to store selected file
+    const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
+    //function called by FileUpload component when file is selected
+    const handleFileSelected = (file: File | null) => {
+        setSelectedFile(file); //update state with stored file
+    }
+    //placeholder for backend logic:
+    const handleUpload = (file: File) => {
+        if(file) {
+            console.log(file.name);
+        } else {
+            console.log("no file upload");
+        }
+    }
     return (
-        <div className="p-10 h-screen">
-            <h1 className="font-bold text-xl pb-4 text-center">Home Page</h1>
+        <div className="p-10 h-screen flex flex-col items-center justify-start">
+            <h1 className="font-extrabold text-4xl pb-4 text-center">Home Page</h1>
+            <FileUpload
+                onFileSelected={handleFileSelected}
+                acceptedFileTypes={['application/json']}
+                buttonText={'Upload File'}/>
+            {/*IF file has been selected*/}
+            {selectedFile && (
+                <div>
+                    {/*display name of file and process*/}
+                    <p>Selected File: {selectedFile?.name}</p>
+                    <button onClick={() => handleUpload(selectedFile)} className="bg-black text-white rounded-lg px-5 py-1">Process and Upload</button>
+                </div>
+            )}
         </div>
     );
 };
